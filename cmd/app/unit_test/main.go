c25f4371ce600b550696661ba75d93be10532f17
James Shufelt <jamesshufeltiii@gmail.com>
2025-08-07 01:26:42 -0500

    adding mainfile stuff

--- cmd/app/unit_test/main.go
+++ cmd/app/unit_test/main.go
@@ -1,5 +1,35 @@
-package unittest
+package main
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+func dosomething(ctx context.Context) {
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("stopping task: %v\n", ctx.Err())
+			return
+		case <- ticker.C:
+		fmt.Println("unit_test is currently running")
+
+		}
+
+	}
+}
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	fmt.Println("starting  server")
+	go dosomething(ctx)
+
+	<- ctx.Done()
+	// for testing purposes
+	time.Sleep(50 * time.Millisecond)
 }
