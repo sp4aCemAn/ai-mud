b8bb2edf9ebd482a31f6eee6051bb45a42475b6e
James Shufelt <jamesshufeltiii@gmail.com>
2025-08-07 02:49:21 -0500

    adding more notes and fixing typos

--- cmd/app/server/main.go
+++ cmd/app/server/main.go
@@ -10,17 +10,19 @@ import (
 // still deciding on how we are structuring this service for right now 
 // we should write components functions to call as go routines in the main function
 // right now i just have this here 
-// we might need to consider writing multiple functions for multiple go routines for processes that block
-// other processes
+// we might need to consider writing multiple functions for multiple go routines 
+// for processes that block other processes
 
-// here is an example function on how where we are are doing stuff to run in our main function 
-func mainloop(ctx context.Context) {
+// here is an example function on to run in our main function  
+// this is our main loop
+// we should follow this pattern for other services that require to be ran in a separate go routine
+func mainloop(ctx context.Context) { // takes context
 	ticker := time.NewTicker(3 * time.Second)
 	defer ticker.Stop()
 
-	for {
+	for { // probebly dont do this 
 		select {
-		case <- ctx.Done():
+		case <- ctx.Done(): // ends in function 
 			fmt.Printf("stopping task: %v\n", ctx.Err())
 			return
 		case <- ticker.C:
